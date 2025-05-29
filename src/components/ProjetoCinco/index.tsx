096558bd4ef79efdd0a085ec13f1750959f3f478
Victor Bonini <victorbonini.vb@gmail.com>
2025-05-29 15:30:59 -0300

    mudança no disclaimer de introdução do projeto 5

--- src/components/ProjetoCinco/index.tsx
+++ src/components/ProjetoCinco/index.tsx
@@ -6,7 +6,7 @@ import { Card, LinkBotao } from './styles'
 const ProjetoCinco = () => (
     <Card>
         <Titulo>Projeto Lista de tarefas</Titulo>
-        <Paragrafo tipo="secundario">Lista de tarefas feita com VueJS</Paragrafo>
+        <Paragrafo tipo="secundario">Lista de tarefas criada apartir de React com TypeScript, fazendo o uso de tecnologias como React-Redux e React-Routes </Paragrafo>
         <LinkBotao href="https://to-do-list-tawny-psi-36.vercel.app/" target="_blank"> To-Do List </LinkBotao>
     </Card>
 )
